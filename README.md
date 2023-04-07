aba98376c825e7ea3296282f4f6507787f6469fd
Naga Lakshmi Konanki <123758623+S562695@users.noreply.github.com>
2023-04-06 23:09:51 -0500

    Modifying changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # s23wb95Konanki
 Hello folks https://s23wb95konanki.onrender.com
 ## Earth
-Oceans_name(String)
-Continents_name(String)
-population(number)
+continents(String)
+Continentnum(String)
+position(number)
--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var EarthRouter = require('./routes/Earth');
-
+var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Earth', EarthRouter);
+app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
